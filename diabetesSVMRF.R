9a4d6c4b5818a7bff5eb7a7f08ff30c6e7eb3b13
rshinoha <37460970+rshinoha@users.noreply.github.com>
2018-05-22 10:12:26 -0500

    Data preprocessing, K-fold set up, outline
    Data preprocessing: changed variables to factors (please check!!!)
    K-fold set up: opted out of creating holdout set, since data is small (n a bit greater than 1000)
    Outline: moved K-folds to step 1
    Big question: feature selection!?!?!?

--- diabetesSVMRF.R
+++ diabetesSVMRF.R
@@ -1,27 +1,77 @@
 library(ggplot2)
+library(data.table)
+library(caret)
 
 data <- read.csv('public_v2_042618.csv')
 
-# 1) Feature selection
+# Seed number for random number generator: seed
+seed <- 1234
+
+# Label name: label
+# (Note: 1 = is diabetic, 2 = not diabetic)
+label <- "DBTS_NEW"
+
+# Drop first column, since it's the index
+data <- data[-c(1)]
+# Print first 10 rows to compare with shuffled data
+head(data)
+
+# Change non-numeric columns to factors
+# Note: not sure about some of the columns I marked as factor should be a
+# factor; please contact me to discuss this
+numericCol <- c("CAPI_WT", "EXAM_WT", "ACASI_WT", "BLOOD_WT", "SPAGE",
+                "HSQ_2", "HSQ_3", "HSQ_4", "HSQ_5", "OHQ_4", "OHQ_5",
+                "ALQ_1","BMI")
+data[!(names(data) %in% numericCol)] <- lapply(data[!(names(data) %in% numericCol)], factor)
+
+# Check if above code worked
+sapply(data, class)
+
+# Be sure that DBTS_NEW class 1 is the "true" variable
+data$DBTS_NEW <- factor(data$DBTS_NEW, levels = c("1", "2"))
+
+# Get index of dependent variable: yindex
+yindex <- grep(label, colnames(data))
+
+# Get the list of colnames: tmpcolname
+tmpcolname <- names(data)
+
+# 1) Cross validation set up
+# ==========================
+# Set up overview:
+# - Shuffle data
+# - Prepare 10-fold CV (no holdout, since small sample)
+
+# Shuffle data
+set.seed(seed)
+data <- data[sample(1:nrow(data)), ]
+# Print first 10 rows of shuffled data
+head(data)
+
+# Show how many people are diabetic
+summary(data$DBTS_NEW)
+
+# Split k = 10 folds (stratify!)
+set.seed(seed)
+folds <- createFolds(factor(data$DBTS_NEW), k = 10, list = FALSE)
+data$fold <- folds
+
+# 2) Feature selection
 # ====================
 # Try to get down to about 15 features (before dummies)
 # Use 3 methods: chi-square, GINI, random forest
 
 # Chi-square
+# For each variable
+# If the variable number isn't yindex
+# Get the p.value
+# For every feature available
+# If the colname isn't
 
 # GINI
 
 # Random Forest
 
-# 2) Cross validation set up
-# ==========================
-# Use 10-fold cross validation and validation set
-
-# Shuffle data
-
-# Separate validation set (stratify!)
-
-# Split 10 folds (stratify!)
 
 # 3) SVM
 # ======
